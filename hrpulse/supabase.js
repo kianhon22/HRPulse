14b88ef734925eb720403450666bfa48456e5efb
tkhabc <93929121+tkhabc@users.noreply.github.com>
2025-03-24 01:11:31 +0800

    Delete hrpulse directory

--- hrpulse/supabase.js
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-// Android emulators cannot access localhost from the computer.
-const SUPABASE_URL = 'http://10.0.2.2:8000'; // Change if running Supabase locally
--- hrpulse/views/about.jsx
+++
@@ -1,7 +0,0 @@
-//about.jsx
-
-import { View } from "react-native";
-
-export default function About() {
-  return <View>About screen</View>;
-}
