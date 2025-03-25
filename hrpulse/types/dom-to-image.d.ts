b3d92ffae04271a0470af0d2693d5d8e0e1c0a73
Tan Kian Hon <tkianhon@gmail.com>
2025-03-26 02:04:18 +0800

    test app

---
+++ hrpulse/types/dom-to-image.d.ts
@@ -0,0 +1,14 @@
+// declare module 'dom-to-image' {
+//   interface Options {
+//     quality?: number;
+//     width?: number;
+//     height?: number;
+//   }
+
+//   interface DomToImage {
+//     toJpeg(node: HTMLElement, options?: Options): Promise<string>;
+//   }
+
+//   const domtoimage: DomToImage;
+//   export default domtoimage;
+// } 
